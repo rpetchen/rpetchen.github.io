ab3468057ea0029a4b8fabc2da005d76c30a01b2
rpetchen <rpetchen@gmu.edu>
2017-04-23 22:23:28 -0400

    4_23

--- Arcade/js/app.js
+++ Arcade/js/app.js
@@ -1,19 +1,39 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+
+var cWidth = 505;
+
+var cHheight = 606;
+
+var allEnemies = [];
+
+var maxEnemies = 10;
+
+var speed = 200
+
+var Enemy = function() {
+    this.y = 300;
+    this.x = 50;
     this.sprite = 'images/enemy-bug.png';
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+this.x += speed * dt;
+
+    if (this.x > cWidth-50) {
+    speed = -speed
+}
+   if(this.x <= cWidth -500) {
+    speed = Math.abs(speed)
+    }
+
+};
+for (var i = 0; i <= maxEnemies; i++) {
+allEnemies[i] = new Enemy();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,6 +44,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+
+var Player = function () {
+    this.x = cWidth/2;
+    this.y = 0;
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function() {}
+
+
+Player.prototype.handleInput= function () {
+
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+};
+
+
+var player = new Player();
 
 
 // Now instantiate your objects.
