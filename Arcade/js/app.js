0b5bc76eb52f99ecab9eb0af4bacd7804a33b47c
rpetchen <rpetchen@gmu.edu>
2017-04-24 22:06:47 -0400

    movement

--- Arcade/js/app.js
+++ Arcade/js/app.js
@@ -7,34 +7,42 @@ var cHheight = 606;
 
 var allEnemies = [];
 
-var maxEnemies = 10;
+var maxEnemies = 4;
 
-var speed = 200
+var speed = 100
+
+var enemyStart = [60, 140, 220 ]
+
+
 
 var Enemy = function() {
-    this.y = 300;
-    this.x = 50;
+    this.y = enemyStart[Math.floor((Math.random() * 3))];
+    this.x = -100;
+    this.enemys = this.x;
     this.sprite = 'images/enemy-bug.png';
-    this.speed = speed;
+    this.speed = speed * Math.floor((Math.random() * 2) + 1);
 };
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
 
-this.x += speed ;
+this.x += this.speed * dt ;
 
-    if (this.x > cWidth-50) {
-    speed = -speed
-}
-   if(this.x <= cWidth -500) {
-    speed = Math.abs(speed)
-    }
+allEnemies = allEnemies.filter(function (enemy) {
+    return enemy.x < cWidth;
+});
+
+for (var i = 0; i < maxEnemies - allEnemies.length; i++) {
+allEnemies.push (new Enemy());
+};
+
 
 };
-for (var i = 0; i <= maxEnemies; i++) {
-allEnemies[i] = new Enemy();
-};
+
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -46,16 +54,30 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 var Player = function () {
-    this.x = cWidth/2;
-    this.y = 0;
+    this.x = 101;
+    this.y = 101*4
     this.sprite = 'images/char-boy.png';
 }
 
 Player.prototype.update = function() {}
 
 
-Player.prototype.handleInput= function () {
+Player.prototype.handleInput= function (key) {
+    switch (key) {
+        case "up":
+            this.y -= 83;
+            break;
+        case "down":
+            this.y += 83;
+            break;
+        case "left":
+            this.x -= 100;
+            break;
+        case "right":
+            this.x += 100;
+    };
 
+console.log(key)
 };
 
 Player.prototype.render = function () {
@@ -64,6 +86,10 @@ Player.prototype.render = function () {
 
 
 var player = new Player();
+
+for (var i = 0; i <= maxEnemies; i++) {
+allEnemies[i] = new Enemy();
+};
 
 
 // Now instantiate your objects.
@@ -74,6 +100,7 @@ var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
